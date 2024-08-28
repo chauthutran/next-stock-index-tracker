83d15494ee057aaf5ef9de26e38f580413eab550
chauthutran <cthutran@gmail.com>
2024-08-28 16:05:25 +0900

    Update useStockData.ts

--- app/lib/hooks/useStockData.ts
+++ app/lib/hooks/useStockData.ts
@@ -13,17 +13,20 @@ const fetcher = (symbols: string[]) => axios.get(`/api/stock-index`, {
 	params: {
 		symbols: symbols.join(",")
 	},
-}).then(res => res);
+}).then(res => res.data);
 
 const useStockData = (symbols: string[]) => {
 	console.log("============= useStockData");
 
-	const response = useSWR(symbols, fetcher, {
-			// refreshInterval: 5 * 1000, //  Fetch data every 5 seconds
-			refreshInterval: 5 * 60 * 60 * 1000, // Fetch data every 5 minutes
-			revalidateOnFocus: true,
-			revalidateOnReconnect: true,
-		});
+	const response =  useSWR(
+		symbols.length > 0 ? `/api/stock-index?symbols=${symbols.join(",")}` : null,
+		() => fetcher(symbols),
+		{
+		  refreshInterval: 5 * 60 * 60 * 1000,
+		  revalidateOnFocus: true,
+		  revalidateOnReconnect: true,
+		}
+	  );
 
 	console.log (response);
 	// const {data, mutate, error, isValidating} = useSWR(symbols, fetcher, {
